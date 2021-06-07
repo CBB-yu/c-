765f4d14898a3cbb50dd1411c87dcde3271f3319
cjz <15768162765@163.com>
2021-06-07 22:01:28 +0800

    default parameters

--- main.cpp
+++ main.cpp
@@ -3,29 +3,15 @@
 
 using namespace std;
 
+//error (int num1 = 321,int num2)
+
+void f(int num1 = 321,int num2 = 123)
+{
+    cout << num1 << " " << num2 <<endl;
+}
 int main()
 {
 
-    string str  = "12131451112";
-
-    int finded = -1;
-    for( int i=0; i<(int)str.length(); i++)
-    {
-            while(1)
-            {
-                finded = str.find_last_of(str[i]);
-                if(finded != -1 && finded != i)     //not found or not self
-                {
-
-                    str.erase(finded,1);
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-    }
-    cout << str <<endl;
+    f(10);
     return 0;
 }
