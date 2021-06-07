753ea59334c9c5ee997324c031be8739a468f165
cjz <15768162765@163.com>
2021-06-07 20:03:29 +0800

    hello world

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
 using namespace std;
+using  std::cout;
+namespace G
+{
+
+    int a = 10;
+}
+namespace G
+{
+    int b = 11;
+}
 
 int main()
 {
 
     cout << "are u ok?" << endl;
+    cout << G::a  << "adad" << endl;
+    cout << G::b <<endl;
     return 0;
 }
